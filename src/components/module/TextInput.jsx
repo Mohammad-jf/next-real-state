183caee32e3fe8c0f56985af6d3992c123969caf
mohammad-jf <m.h.jalifathi@gmail.com>
2024-07-06 03:22:28 +0330

    textinput

---
+++ src/components/module/TextInput.jsx
@@ -0,0 +1,30 @@
+import styles from './textInput.module.css'
+
+const TextInput = ({ title, name, profileData, setProfileData, textArea = false }) => {
+
+    const changeHandler = (e) => {
+        setProfileData({
+            ...profileData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return (
+        <div className={styles.container}>
+            <p>{title}</p>
+            {textArea ? (
+                <textarea
+                    name={name}
+                    type='text'
+                    value={profileData[name]}
+                    onChange={changeHandler}></textarea>
+            ) : <input
+                name={name}
+                type='text'
+                value={profileData[name]}
+                onChange={changeHandler} />}
+        </div>
+    )
+}
+
+export default TextInput 
