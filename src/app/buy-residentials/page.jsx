ed471801a4943632018a29440dc5afbf9b39ed94
mohammad-jf <m.h.jalifathi@gmail.com>
2024-07-30 15:00:21 +0330

    update packeges

--- src/app/buy-residentials/page.jsx
+++ src/app/buy-residentials/page.jsx
@@ -5,7 +5,6 @@ const Residential = async ({ searchParams }) => {
   const res = await fetch("http://localhost:3000/api/profile", {
     cache: "no-store",
   });
-
   const data = await res.json();
   if (data.error) return <h3> مشکلی پیش امده است</h3>;
 
